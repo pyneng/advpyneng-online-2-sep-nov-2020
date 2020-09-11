13a0a578ec8bc0d8735d6ff3626780b30494724b
pyneng <pyneng.course@gmail.com>
2020-09-11 11:34:32 +0000

    Black 11_oop_method_decorators

--- exercises/11_oop_method_decorators/test_task_11_1a.py
+++ exercises/11_oop_method_decorators/test_task_11_1a.py
@@ -1,7 +1,8 @@
 import pytest
 import task_11_1a
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 
 from common_functions import check_class_exists, check_attr_or_method
 
@@ -14,18 +15,20 @@ if not isinstance(__loader__, AssertionRewritingHook):
 
 
 def test_class_created():
-    '''Проверяем, что класс создан'''
-    check_class_exists(task_11_1a, 'IPv4Network')
+    """Проверяем, что класс создан"""
+    check_class_exists(task_11_1a, "IPv4Network")
 
 
 def test_attributes_created():
-    '''
+    """
     Проверяем, что у класса есть метод from_tuple:
-    '''
-    check_attr_or_method(task_11_1a.IPv4Network, method='from_tuple')
+    """
+    check_attr_or_method(task_11_1a.IPv4Network, method="from_tuple")
 
 
 def test_create_instance_from_tuple():
-    '''Проверяем работу объекта'''
-    net = task_11_1a.IPv4Network.from_tuple(('100.1.1.0', 25))
-    assert isinstance(net, task_11_1a.IPv4Network), "Метод from_tuple должен создавать экземпляр класса IPv4Network"
+    """Проверяем работу объекта"""
+    net = task_11_1a.IPv4Network.from_tuple(("100.1.1.0", 25))
+    assert isinstance(
+        net, task_11_1a.IPv4Network
+    ), "Метод from_tuple должен создавать экземпляр класса IPv4Network"
