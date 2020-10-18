83c99044b3f312b60c290a394bc922b0c2cbad29
pyneng <pyneng.course@gmail.com>
2020-10-18 05:22:56 +0000

    lecture 12

--- examples/11_oop_method_decorators/class_ipaddress.py
+++ examples/11_oop_method_decorators/class_ipaddress.py
@@ -32,4 +32,24 @@ class IPAddress:
         self._mask = value
 
 
+# проверка в init
+class IPAddress:
+    def __init__(self, ip, mask):
+        self._ip = ip
+        self.mask = mask
+
+    @property  # этот метод превращается в getter
+    def mask(self):
+        print("getter")
+        return self._mask
+
+    @mask.setter
+    def mask(self, value):
+        print("setter")
+        if not isinstance(value, int):
+            raise TypeError("Значение должно быть числом")
+        if not 8 <= value <= 32:
+            raise ValueError("Значение должно быть в диапазоне 8 - 32")
+        self._mask = value
+
 ### Больше примеров property в файле class_book_property.py
